600378c92326b95e9b16eca4c0a2611b0ee38893
RawabiA <rawabi94.a@gmail.com>
2018-05-20 08:21:44 +0300

    add alt

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", restaurant.name);
+  image.setAttribute("role", "img");
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
